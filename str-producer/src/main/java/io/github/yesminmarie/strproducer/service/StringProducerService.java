c61c60ae3626c484a898cd78b63413a3a1c47b8c
Yesmin Lahoud <ymslahoud@gmail.com>
2023-01-15 17:14:56 -0300

    KafkaTemplate, Service e Resource

---
+++ str-producer/src/main/java/io/github/yesminmarie/strproducer/service/StringProducerService.java
@@ -0,0 +1,16 @@
+package io.github.yesminmarie.strproducer.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        kafkaTemplate.send("str-topic", message);
+    }
+}
