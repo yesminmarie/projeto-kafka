8e840d1fec1e479b0d089af815fc27cfc409b628
Yesmin Lahoud <ymslahoud@gmail.com>
2023-01-15 13:31:53 -0300

    str-producer started

---
+++ str-producer/src/test/java/io/github/yesminmarie/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yesminmarie.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
