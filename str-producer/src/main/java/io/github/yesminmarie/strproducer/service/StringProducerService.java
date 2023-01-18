73d22a7077e5f67e4979f890ccd6c26dfa88b263
Yesmin Lahoud <ymslahoud@gmail.com>
2023-01-18 11:05:03 -0300

    adicionando callBack no metodo sendMessage

--- str-producer/src/main/java/io/github/yesminmarie/strproducer/service/StringProducerService.java
+++ str-producer/src/main/java/io/github/yesminmarie/strproducer/service/StringProducerService.java
@@ -1,9 +1,14 @@
 package io.github.yesminmarie.strproducer.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -11,6 +16,17 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message){
-        kafkaTemplate.send("str-topic", message);
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("str-topic", message);
+
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                log.info("Send message with success {}", message);
+                log.info("Partition {}, Offset {}",
+                        result.getRecordMetadata().partition(),
+                        result.getRecordMetadata().offset());
+            } else {
+                log.error("Error send message");
+            }
+        });
     }
 }
